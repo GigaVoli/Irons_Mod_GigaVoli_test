ee2f2ed27258d37ac03248e2244e9e39b9de4bdf
GigaVoli <zach.dean.williams@outlook.com>
2024-09-21 12:46:07 -0700

    Initial Commit

---
+++ src/main/java/io/redspace/volis_spells_and_addons/data/CustomSpellSyncData.java
@@ -0,0 +1,187 @@
+package io.redspace.volis_spells_and_addons.data;
+
+import io.redspace.ironsspellbooks.api.entity.IMagicEntity;
+import io.redspace.ironsspellbooks.api.magic.LearnedSpellData;
+import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
+import io.redspace.ironsspellbooks.capabilities.magic.SyncedSpellData;
+import io.redspace.ironsspellbooks.gui.overlays.SpellSelection;
+import io.redspace.ironsspellbooks.network.ClientboundSyncEntityData;
+import io.redspace.ironsspellbooks.network.ClientboundSyncPlayerData;
+import io.redspace.ironsspellbooks.player.SpinAttackType;
+import io.redspace.ironsspellbooks.setup.Messages;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.syncher.EntityDataSerializer;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.LivingEntity;
+import org.jetbrains.annotations.Nullable;
+
+public class CustomSpellSyncData extends SyncedSpellData {
+    public static final long BLOOD_BARRIER = 256L;
+    private int hitsRemaining;
+    private @Nullable LivingEntity livingEntity;
+    private boolean isCasting;
+    private String castingSpellId;
+    private int castingSpellLevel;
+    private long syncedEffectFlags;
+    private long localEffectFlags;
+    private float heartStopAccumulatedDamage;
+    private int evasionHitsRemaining;
+    private SpinAttackType spinAttackType;
+    private LearnedSpellData learnedSpellData;
+    private SpellSelection spellSelection;
+    private String castingEquipmentSlot;
+    public static final EntityDataSerializer<CustomSpellSyncData> SYNCED_SPELL_DATA = new EntityDataSerializer.ForValueType<CustomSpellSyncData>() {
+        public void write(FriendlyByteBuf buffer, CustomSpellSyncData data) {
+            buffer.writeInt(data.hitsRemaining);
+            data.learnedSpellData.writeToBuffer(buffer);
+            data.spellSelection.writeToBuffer(buffer);
+        }
+
+        public void write(FriendlyByteBuf pBuffer, SyncedSpellData pValue) {
+
+        }
+
+        public CustomSpellSyncData read(FriendlyByteBuf buffer) {
+            CustomSpellSyncData data = new CustomSpellSyncData(buffer.readInt());
+            data.isCasting = buffer.readBoolean();
+            data.hitsRemaining = buffer.readInt();
+            data.learnedSpellData.readFromBuffer(buffer);
+            data.spellSelection.readFromBuffer(buffer);
+            return data;
+        }
+    };
+
+    public CustomSpellSyncData(int serverPlayerId) {
+        super(serverPlayerId);
+        this.hitsRemaining = 0;
+        this.learnedSpellData = new LearnedSpellData();
+        this.spellSelection = new SpellSelection();
+    }
+
+    public CustomSpellSyncData(LivingEntity livingEntity) {
+        this(livingEntity == null ? -1 : livingEntity.getId());
+        this.livingEntity = livingEntity;
+    }
+
+    public void saveNBTData(CompoundTag compound) {
+        compound.putInt("hitsRemaining", this.hitsRemaining);
+        this.learnedSpellData.saveToNBT(compound);
+        compound.put("spellSelection", this.spellSelection.serializeNBT());
+    }
+
+    public void loadNBTData(CompoundTag compound) {
+        this.hitsRemaining = compound.getInt("hitsRemaining");
+    }
+
+    public boolean hasEffect(long effectFlags) {
+        return (this.syncedEffectFlags & effectFlags) == effectFlags;
+    }
+
+    public String getCastingEquipmentSlot() {
+        return this.castingEquipmentSlot;
+    }
+
+    public boolean hasLocalEffect(long effectFlags) {
+        return (this.localEffectFlags & effectFlags) == effectFlags;
+    }
+
+    public void addLocalEffect(long effectFlags) {
+        this.localEffectFlags |= effectFlags;
+    }
+
+    public void removeLocalEffect(long effectFlags) {
+        this.localEffectFlags &= ~effectFlags;
+    }
+
+    public SpellSelection getSpellSelection() {
+        return this.spellSelection;
+    }
+
+    public void setSpellSelection(SpellSelection spellSelection) {
+        this.spellSelection = spellSelection;
+        this.doSync();
+    }
+
+    public void learnSpell(AbstractSpell spell) {
+        this.learnedSpellData.learnedSpells.add(spell.getSpellResource());
+        this.doSync();
+    }
+
+    public void forgetAllSpells() {
+        this.learnedSpellData.learnedSpells.clear();
+        this.doSync();
+    }
+
+    public boolean isSpellLearned(AbstractSpell spell) {
+        return !spell.needsLearning() || this.learnedSpellData.learnedSpells.contains(spell.getSpellResource());
+    }
+
+    public int getHitsRemaining() {
+        return this.hitsRemaining;
+    }
+
+    public void subtractHit() {
+        --this.hitsRemaining;
+        this.doSync();
+    }
+
+    public void setHitsRemaining(int hitsRemaining) {
+        this.hitsRemaining = hitsRemaining;
+        this.doSync();
+    }
+
+    public void addEffects(long effectFlags) {
+        this.syncedEffectFlags |= effectFlags;
+        this.doSync();
+    }
+
+    public void removeEffects(long effectFlags) {
+        this.syncedEffectFlags &= ~effectFlags;
+        this.doSync();
+    }
+
+    public void doSync() {
+        LivingEntity var3 = this.livingEntity;
+        if (var3 instanceof ServerPlayer serverPlayer) {
+            Messages.sendToPlayer(new ClientboundSyncPlayerData(this), serverPlayer);
+            Messages.sendToPlayersTrackingEntity(new ClientboundSyncPlayerData(this), serverPlayer);
+        } else {
+            var3 = this.livingEntity;
+            if (var3 instanceof IMagicEntity abstractSpellCastingMob) {
+                Messages.sendToPlayersTrackingEntity(new ClientboundSyncEntityData(this, abstractSpellCastingMob), this.livingEntity);
+            }
+        }
+
+    }
+
+    public void syncToPlayer(ServerPlayer serverPlayer) {
+        Messages.sendToPlayer(new ClientboundSyncPlayerData(this), serverPlayer);
+    }
+
+    public boolean isCasting() {
+        return this.isCasting;
+    }
+
+    public String getCastingSpellId() {
+        return this.castingSpellId;
+    }
+
+    public int getCastingSpellLevel() {
+        return this.castingSpellLevel;
+    }
+
+    protected SyncedSpellData clone() {
+        return new SyncedSpellData(this.livingEntity);
+    }
+
+    public String toString() {
+        return String.format("isCasting:%s, spellID:%s, spellLevel:%d, effectFlags:%d", this.isCasting, this.castingSpellId, this.castingSpellLevel, this.syncedEffectFlags);
+    }
+
+    public CustomSpellSyncData getPersistentData() {
+        CustomSpellSyncData persistentData = new CustomSpellSyncData(this.livingEntity);
+        persistentData.learnedSpellData.learnedSpells.addAll(this.learnedSpellData.learnedSpells);
+        return persistentData;
+    }
+}
--- src/main/java/io/redspace/volis_spells_and_addons/effects/BloodBarrierEffect.java
+++ src/main/java/io/redspace/volis_spells_and_addons/effects/BloodBarrierEffect.java
@@ -1,13 +1,22 @@
 package io.redspace.volis_spells_and_addons.effects;
 
+import io.redspace.ironsspellbooks.api.entity.IMagicEntity;
+import io.redspace.ironsspellbooks.api.magic.LearnedSpellData;
 import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
 import io.redspace.ironsspellbooks.capabilities.magic.MagicManager;
 import io.redspace.ironsspellbooks.capabilities.magic.SyncedSpellData;
 import io.redspace.ironsspellbooks.datagen.DamageTypeTagGenerator;
 import io.redspace.ironsspellbooks.effect.AbyssalShroudEffect;
 import io.redspace.ironsspellbooks.effect.EvasionEffect;
 import io.redspace.ironsspellbooks.effect.MagicMobEffect;
+import io.redspace.ironsspellbooks.gui.overlays.SpellSelection;
+import io.redspace.ironsspellbooks.network.ClientboundSyncEntityData;
+import io.redspace.ironsspellbooks.network.ClientboundSyncPlayerData;
+import io.redspace.ironsspellbooks.player.SpinAttackType;
 import io.redspace.ironsspellbooks.registries.MobEffectRegistry;
+import io.redspace.ironsspellbooks.setup.Messages;
+import io.redspace.volis_spells_and_addons.data.CustomSpellSyncData;
 import io.redspace.volis_spells_and_addons.registry.EffectRegistry;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.arguments.EntityAnchorArgument;
@@ -15,7 +24,9 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.syncher.EntityDataSerializer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.DamageTypeTags;
@@ -30,6 +41,7 @@ import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 
 public class BloodBarrierEffect extends MagicMobEffect {
 
@@ -49,15 +61,15 @@ public class BloodBarrierEffect extends MagicMobEffect {
     public void addAttributeModifiers(LivingEntity pLivingEntity, AttributeMap pAttributeMap, int pAmplifier) {
         super.addAttributeModifiers(pLivingEntity, pAttributeMap, pAmplifier);
         MagicData.getPlayerMagicData(pLivingEntity).getSyncedData().addEffects(256L);
-        CustomSpellSyncData.setBloodCounter(pAmplifier);
+        ((CustomSpellSyncData) MagicData.getPlayerMagicData(pLivingEntity).getSyncedData()).setHitsRemaining(pAmplifier);
     }
 
     public static boolean doEffect(LivingEntity livingEntity, DamageSource damageSource) {
         if (!livingEntity.level().isClientSide && !damageSource.is(DamageTypeTags.IS_FALL) && !damageSource.is(DamageTypeTags.BYPASSES_INVULNERABILITY) && !damageSource.is(DamageTypeTagGenerator.BYPASS_EVASION)) {
-            SyncedSpellData data = MagicData.getPlayerMagicData(livingEntity).getSyncedData();
+            CustomSpellSyncData data =(CustomSpellSyncData) MagicData.getPlayerMagicData(livingEntity).getSyncedData();
 
-            CustomSpellSyncData.decrement();
-            if (CustomSpellSyncData.getBloodCounter() < 1) {
+            data.subtractHit();
+            if (data.getHitsRemaining() < 1) {
                 livingEntity.removeEffect((MagicMobEffect) EffectRegistry.BLOOD_BARRIER.get());
             }
             return true;
@@ -65,35 +77,5 @@ public class BloodBarrierEffect extends MagicMobEffect {
             return false;
         }
     }
-    public class CustomSpellSyncData extends SyncedSpellData{
 
-        public static final long BLOOD_BARRIER = 256;
-        private int hitsRemaining;
-
-        public void decrement() {
-            hitsRemaining --;
-        }
-        public int getBloodCounter() {
-            return hitsRemaining;
-        }
-
-        public void setBloodCounter(int hitsRemaining) {
-            this.hitsRemaining = hitsRemaining;
-        }
-        public CustomSpellSyncData(LivingEntity livingEntity) {
-            super(livingEntity);
-            hitsRemaining = 0;
-        }
-        public SyncedSpellData read(FriendlyByteBuf buffer) {
-            var data = new SyncedSpellData(buffer.readInt());
-            hitsRemaining = buffer.readInt();
-            return data;
-        }
-        public void saveNBTData(CompoundTag compound) {
-            compound.putFloat("bloodHitsRemaining", this.hitsRemaining);
-            learnedSpellData.saveToNBT(compound);
-            compound.put("spellSelection", this.spellSelection.serializeNBT());
-            //SpinAttack not saved
-        }
     }
-}
